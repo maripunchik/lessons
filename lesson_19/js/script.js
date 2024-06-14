f7293817b16f489f9c141d7f167e179d7ebd8598
maripunchik <162891408+maripunchik@users.noreply.github.com>
2024-06-14 09:36:07 +0200

    Delete lesson_19 directory

--- lesson_19/js/script.js
+++
@@ -1,8 +0,0 @@
-
-
-document.addEventListener('click', function (e) {
-	const targetElement = e.target;
-	if (targetElement.closest('.top-header__icon')) {
-		document.documentElement.classList.toggle('menu-open');
-	}
-})
